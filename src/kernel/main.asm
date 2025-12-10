352e7d987b3c20ae1262a0498f4049c422b19fd5
Luneux <yanselpedraza@gmail.com>
2025-12-09 22:18:31 -0600

    Add emulator configs and improve bootloader/kernel
    Added Bochs and QEMU configuration files and scripts for easier testing and debugging. Enhanced bootloader and kernel assembly code with improved disk routines, error handling, and more robust string printing. Updated boot messages and FAT12 header details for clarity and consistency.

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,38 +1,54 @@
 org 0x7C00
 bits 16
 
+
 %define ENDL 0x0D, 0x0A
+
 
 start:
     jmp main
 
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
 puts:
+    ; save registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
-    lodsb              ; load next byte into AL
-    or al, al          ; check for null terminator
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
 
-    mov ah, 0x0e
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
     int 0x10
 
     jmp .loop
 
 .done:
+    pop bx
     pop ax
-    pop si
+    pop si    
     ret
+    
 
 main:
-    mov ax, 0
+    ; setup data segments
+    mov ax, 0           ; can't set ds/es directly
     mov ds, ax
     mov es, ax
-
+    
+    ; setup stack
     mov ss, ax
-    mov sp, 0x7C00
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
 
+    ; print hello world message
     mov si, msg_hello
     call puts
 
@@ -41,7 +57,10 @@ main:
 .halt:
     jmp .halt
 
-msg_hello: db 'Hello World!', ENDL, 0
 
-times 510 - ($-$$) db 0
-dw 0xaa55
+
+msg_hello: db 'Hello world!', ENDL, 0
+
+
+times 510-($-$$) db 0
+dw 0AA55h
