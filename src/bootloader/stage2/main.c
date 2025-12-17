fc47877e846eea1f6133a84f3b89f57f2f20151e
lun3ux <yanselpedraza@gmail.com>
2025-12-17 15:58:59 -0600

    Added keyboard functionality in C

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -2,6 +2,7 @@
 #include "stdio.h"
 #include "disk.h"
 #include "fat.h"
+#include "bioshelpers.h"
 
 void far* g_data = (void far*)0x00500200;
 
@@ -51,7 +52,21 @@ void _cdecl cstart_(uint16_t bootDrive)
     }
     FAT_Close(fd);
 
-    
+    puts("\r");
+    puts("   ,,                                                                          ");
+    puts("  `7MM                                                                           ");
+    puts("   MM                                                                           ");
+    puts("   MM `7MM  `7MM  `7MMpMMMb.  .gP\"Ya       ,pW\"Wq.  ,pP\"Ybd                        ");
+    puts("MM   MM    MM    MM    MM ,M'   Yb     6W'   `Wb 8I   `\"                        ");
+    puts("MM   MM    MM    MM    MM 8M\"\"\"\"\"\"     8M     M8 `YMMMa.                       ");
+    puts(" MM   MM    MM    MM    MM YM.    ,     YA.   ,A9 L.   I8                      ");
+    puts(".JMML. `Mbod\"YML..JMML  JMML.`Mbmmd'      `Ybmd9'  M9mmmP'                      ");
+    puts("                                                                                 ");
+    puts("ALPHA 1.0.0                                               ");
+    puts("\r");
+
+    checkKeyStroke();
+
 
 end:
     for (;;);
